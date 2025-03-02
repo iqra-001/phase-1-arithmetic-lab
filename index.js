dc47ec996412463e8c63bd5998983fbeaa4f97ec
iqra <abdikarimiqra@gmail.com>
2025-03-02 19:54:56 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// function multiply(num1, num2) {
+//     let num1 = 31;
+//     let num2 = 2;
+// return ("num1 * num2"); // 62
+//   }
+
+// Math.floor (Math.random())
 
 // Write your code here
+// Create a variable called multiply set to an equation that will multiply num1 and num2 to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;  // This will give 62
+
+// Create a variable called random that generates a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;  // Generates a random integer greater than 0
+
+// Create a variable called mod set to an equation that calculates the remainder of dividing num3 by num4, the remainder should be 4
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;  // This will give 4
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20
+let max = Math.max(5, 10, 15, 20);  // Finds the maximum number, which is 20
